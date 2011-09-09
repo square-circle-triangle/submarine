78a1190ca1e69001d0f63e9b8d1a2b233ffb7777
Nick Marfleet <nick@sct.com.au>
2011-09-09 19:45:20 +1000

    initial commit

---
+++ lib/submarine/string.rb
@@ -0,0 +1,19 @@
+class String
+  def requires_postrendering?
+    !!(self =~ /\{\{\s*([\w]+)\s*(\|([^{}]*))?\}\}/)
+  end
+  
+  def postrender(replacements)
+    gsub(/\{\{\s*([\w]+)\s*(\|([^{}]*))?\}\}/i) do |match|
+      key = $1.downcase.to_sym
+      default = $3 || ""
+
+      if replacements.has_key?(key)
+        k = replacements[key]
+        (k.nil? || k == '') ? replacements[key].strip : default.strip
+      else
+        $&
+      end
+    end
+  end
+end
